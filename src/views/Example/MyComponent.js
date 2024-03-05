a974f1d49904e71355411a53a4ef801996aaecba
ThanhTu26092002 <lethanhtu810@email.com>
2024-03-05 09:17:28 +0700

    state và sự kiện handleOnchange

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,35 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  /*
+     JSX: cho phép viết html  trong file javascript
+     state: giá trị của nó thay đổi thì ct sẽ dc reder lại 
+  
+  */
+
+  state = {
+    name: "Nguyen Thi Ha",
+    address: "Dai Phong",
+  };
+  handleOnchangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
   render() {
-    return <div>Hello </div>;
+    return (
+      <React.Fragment>
+        <div className="first">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnchangeName(event)}
+          />
+          {console.log("my name is", this.state.name)}
+        </div>
+        <div className="second">Hello, My name is {this.state.name}</div>
+      </React.Fragment>
+    );
   }
 }
 export default MyComponent;
