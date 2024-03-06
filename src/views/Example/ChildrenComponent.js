884609c821f37cdd779dbe20a0d02bb907bf2e39
ThanhTu26092002 <lethanhtu810@email.com>
2024-03-06 09:13:01 +0700

    sử dụng function cpn thay cho class cpn

--- src/views/Example/ChildrenComponent.js
+++ src/views/Example/ChildrenComponent.js
@@ -1,53 +1,41 @@
 import React from "react";
+//ĐỊNH NGHĨA CLASS FUNCITON
+// class ChildrenComponent extends React.Component {
+//   render() {
+//     console.log("check props ", this.props);
+//     let { name, age, address, Jobs } = this.props;
+//     return (
+//       <>
+//         <div className="Jobs">
+//           {Jobs.map((item, index) => {
+//             return (
+//               <div key={item.id}>
+//                 {item.name} - {item.address}
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
-class ChildrenComponent extends React.Component {
-  /*
-     JSX: cho phép viết html  trong file javascript
-     state: giá trị của nó thay đổi thì ct sẽ dc reder lại 
-  
-  */
-  state = {
-    firstName: "",
-    lastName: "",
-  };
-  handleOnchangeFirstName = (event) => {
-    this.setState({
-      firstName: event.target.value,
-    });
-  };
-  handleOnchangeLastName = (event) => {
-    this.setState({
-      lastName: event.target.value,
-    });
-  };
-  handleOnclickInput = (event) => {
-    alert(this.state.firstName + this.state.lastName);
-  };
-  render() {
-    console.log("check props ", this.props);
-    let { name, age, address, Jobs } = this.props;
-    return (
-      <>
-        {/* <div className="Joblist">
-          {Jobs.map((item, index) => {
-            return (
-              <div key={item.id}>
-                {item.title}-{item.salary}
-              </div>
-            );
-          })}
-        </div> */}
-        <div className="Jobs">
-          {Jobs.map((item, index) => {
-            return (
-              <div key={item.id}>
-                {item.name} - {item.address}
-              </div>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
-}
+//ĐỊNH NGHĨA FUNCITON COMPONENTS
+const ChildrenComponent = (props) => {
+  let { name, age, address, Jobs } = props;
+
+  return (
+    <>
+      <div className="Jobs">
+        {Jobs.map((item, index) => {
+          return (
+            <div key={item.id}>
+              {item.name} - {item.address}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 export default ChildrenComponent;
